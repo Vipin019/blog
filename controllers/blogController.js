eff97d730407c2dec3eee1fd114799fe3b44bb6d
Vipin019 <vkpmail019@gmail0com>
2023-04-07 20:28:33 +0530

    createBlogController is complet

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -27,7 +27,34 @@ exports.getAllBlogController = async (req, res) => {
 };
 
 //Create blog
-exports.createBlogController = () => {};
+exports.createBlogController = async (req, res) => {
+  try {
+    const { title, description, image } = req.body;
+    if (!title || !description) {
+      return res.status(400).send({
+        success: false,
+        message: "title and description is required",
+      });
+    }
+    let newBlog;
+    if (!image) {
+      newBlog = new blogModel({ title, description });
+    } else {
+      newBlog = new blogModel({ title, description, image });
+    }
+    await newBlog.save();
+    return res.status(200).send({
+      success: true,
+      message: "Blog created successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error in createBlogController",
+    });
+  }
+};
 
 //update blog
 exports.updateBlogController = () => {};
